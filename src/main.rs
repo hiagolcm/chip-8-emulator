2c4f718a7599af4afebadb7a8f8ade0c51785e19
Hiago Lucas Cardeal de Melo Silva <hcardealdemelo@gmail.com>
2026-06-15 17:21:32 -0300

    add set display method

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,22 @@ struct Timer(u8);
 
 struct Screen([[bool; 64]; 32]);
 
+struct Display {
+    screen: Screen
+}
+
+struct Coordinate((u16, u16));
+
+impl Display {
+    fn set(&mut self, coordinate: Coordinate) {
+        let x = (coordinate.0.0 % 64 ) as usize;
+        let y = (coordinate.0.1  % 32 ) as usize;
+        let current_value = self.screen.0[x][y];
+
+        self.screen.0[x][y] = if current_value { false } else { true };
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
